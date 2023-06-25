9d121a38cd5439f28e9f92577197dfd0196d4189
NetologySamir0102 <Molamusov@yandex.ru>
2023-06-25 22:53:14 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+                System.out.println("Мили за рубли");
+        int amount = 11300; //стоимость билета
+        System.out.printf("Стоимость билета");
+
+        int i = 20; // за каждые 20-рублей 1-миля
+        System.out.println ("за каждые 20-рублей ");
+        int ml = 1; // 1-бонусная миля
+        System.out.println("1-бонусная миля");
+        int bonus = 1;
+
+        int finish = amount / 100*ml;
+        System.out.println("Итоговые бонусные мили равны="  + finish);}
+
+}
+
+
